185a72427babdcfd873b9e8a2ead0cc30e769465
Samarth Agarwal <44058680+Thecapable@users.noreply.github.com>
2020-02-14 16:46:29 +0530

    Updated comments for better understanding of code

--- ImgInput.py
+++ ImgInput.py
@@ -1,3 +1,4 @@
+#importing necessary packages
 import cv2
 from darkflow.net.build import TFNet
 import matplotlib.pyplot as plt
@@ -13,7 +14,9 @@ def image(a):
     options = {
         'model': 'cfg/yolo.cfg',
         'load': 'bin/yolov2.weights',
-        'threshold': 0.4,
+        'threshold': 0.4,  #threshold value decides above what confidence level the objects need to be detected
+        #Higher Threshold equals Lesser chance of inaccurate results. 
+        #You can change the threshold value to anywhere between 0-1 and see the changes it makes yourself
     }
     colors = [tuple(255 * np.random.rand(3)) for _ in range(10)]
 
@@ -31,7 +34,7 @@ def image(a):
             label = result['label']
             confidence = result['confidence']
             text = '{}: {:.0f}%'.format(label, confidence * 100)
-            img = cv2.rectangle(img, tl, br, color, 3)
+            img = cv2.rectangle(img, tl, br, color, 3)  #for creating the bounding boxes around objects
             img = cv2.putText(img, label, tl, cv2.FONT_HERSHEY_COMPLEX,2, tuple(255 * np.random.rand(3)),2) 
             
             engine.say(label)
